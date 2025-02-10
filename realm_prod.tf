05f6b2e22fb692a5e502c2d8d328ac08fe5af339
Ondrej Sika <ondrej@ondrejsika.com>
2025-02-10 10:41:42 +0100

    feat: Add themes

--- realm_prod.tf
+++ realm_prod.tf
@@ -3,6 +3,7 @@ resource "keycloak_realm" "prod" {
   enabled                = true
   display_name           = "SSO PROD"
   reset_password_allowed = true
+  login_theme            = "sika-io"
   smtp_server {
     host = "maildev-smtp.example-keycloak"
     port = "25"
--- realm_test.tf
+++ realm_test.tf
@@ -3,6 +3,7 @@ resource "keycloak_realm" "test" {
   enabled                = true
   display_name           = "SSO TEST"
   reset_password_allowed = true
+  login_theme            = "ondrejsika"
   smtp_server {
     host = "maildev-smtp.example-keycloak"
     port = "25"
